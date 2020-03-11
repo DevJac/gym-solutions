a074436aaba6aa7d0e334cf8242f0fbc93f6afe4
Devin Jacobs <dbuttonz@gmail.com>
2020-03-10 18:00:21 -0600

    Move SARS into its own module

--- src/ppo_lander.jl
+++ src/ppo_lander.jl
@@ -5,47 +5,11 @@ using OpenAIGym
 using Plots
 using Printf
 using ProgressMeter
-using Test: @test
+using Sars
 
 pyplot()
 
 const env = GymEnv(:LunarLander, :v2)
-
-mutable struct SARS{S, A}
-    s  :: S
-    a  :: A
-    r  :: Float32
-    q  :: Union{Nothing, Float32}
-    s′ :: S
-    f  :: Bool
-end
-
-function fill_q!(sars; discount_factor=1)
-    q = 0
-    for i in length(sars):-1:1
-        q *= discount_factor
-        if sars[i].f
-            q = 0
-        end
-        q += sars[i].r
-        sars[i].q = q
-    end
-end
-
-function test_fill_q()
-    sars = [
-        SARS(nothing, nothing, 1f0, nothing, nothing, false),
-        SARS(nothing, nothing, 1f0, nothing, nothing, false),
-        SARS(nothing, nothing, 1f0, nothing, nothing, true),
-        SARS(nothing, nothing, 1f0, nothing, nothing, false),
-        SARS(nothing, nothing, 1f0, nothing, nothing, true)]
-    fill_q!(sars)
-    @test sars[1].q == 3
-    @test sars[2].q == 2
-    @test sars[3].q == 1
-    @test sars[4].q == 2
-    @test sars[5].q == 1
-end
 
 struct Policy{Π, Q, V} <: AbstractPolicy
     π :: Π
@@ -236,9 +200,3 @@ function train_until_reward!(policy, stop_reward; fancy_output=false, save_polic
     end
     policy
 end
-
-function run_tests()
-    test_fill_q()
-end
-
-run_tests()
