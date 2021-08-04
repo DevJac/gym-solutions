438eca1081be963110513759848877f6240323ba
DevJac <dbuttonz@gmail.com>
2021-08-04 07:10:34 -0600

    SAC fixes; but still not working?

--- sac.jl
+++ sac.jl
@@ -11,6 +11,13 @@ OpenAIGym.sample(set::DiscreteSet, weights) = sample(set.items, weights)
 
 const env = GymEnv(:LunarLander, :v2)
 
+function polyak_average!(a, b, τ=0.05)
+    for (pa, pb) in zip(params(a), params(b))
+        pa .*= 1 - τ
+        pa .+= pb * τ
+    end
+end
+
 function flatten(x)
     reshape(x, length(x))
 end
@@ -48,8 +55,9 @@ end
 function make_π_network()
     Chain(
         nonans("π input"),
-        Dense(8, 10),
-        Dense(10, 4),
+        Dense(4, 600, relu),
+        Dense(600, 200, relu),
+        Dense(200, 2),
         softmax,
         nonans("π output"))
 end
@@ -57,9 +65,9 @@ end
 function make_q_network()
     Chain(
         nonans("q input"),
-        Dense(8, 10),
-        Dense(10, 4),
-        softmax,
+        Dense(4, 600, relu),
+        Dense(600, 200, relu),
+        Dense(200, 2),
         nonans("q output"))
 end
 
@@ -70,14 +78,23 @@ function Q(policy, s, a)
         policy.q2(s)[a+1])
 end
 
-const q_opt = RMSProp(0.001)
+function Q_targ(policy, s, a)
+    # 0 is a valid action. Add 1 to move to Julia's 1-based indexing.
+    min(
+        policy.q1_targ(s)[a+1],
+        policy.q2_targ(s)[a+1])
+end
+
+const q1_opt = RMSProp(0.001)
+const q2_opt = RMSProp(0.001)
 const π_opt = RMSProp(0.001)
+
+const γ = 0.99
+const α = 0.03
 
 function train!(policy, replay_buffer)
     training_transitions = sample(replay_buffer, 100)
     q_target::Vector{Float32} = collect(map(training_transitions) do t
-        γ = 0.99
-        α = 0.01
         if t.d
             t.r
         else
@@ -86,7 +103,7 @@ function train!(policy, replay_buffer)
             # Subtract 1 to move from 1-based Julia indexing to 0-based Python / Gym indexing
             a′ = a′_sample_index - 1
             a′_prob = a′_probs[a′_sample_index]
-            Q_term = Q(policy, t.s′, a′)
+            Q_term = Q_targ(policy, t.s′, a′)
             entropy_term = α * log(a′_prob)
             @debug "y terms" Q_term entropy_term
             t.r + γ * (Q_term - entropy_term)
@@ -98,7 +115,7 @@ function train!(policy, replay_buffer)
     @debug "pre q training" q_target
     @debug "" s_stack
     @debug "" a_stack
-    for q_network in (policy.q1, policy.q2)
+    for (q_opt, q_network) in ((q1_opt, policy.q1), (q2_opt, policy.q2))
         q_params = params(q_network)
         loss, grads = valgrad(q_params) do
             q_out::Matrix{Float32} = q_network(s_stack)
@@ -108,12 +125,12 @@ function train!(policy, replay_buffer)
             error::Vector{Float32} = (qav - q_target).^2
             mean(error)
         end
+        @debug "q loss" loss
         Flux.Optimise.update!(q_opt, q_params, grads)
     end
     π_params = params(policy.π)
     loss, grads = valgrad(π_params) do
         -mean(training_transitions) do t
-            α = 0.01
             a_probs = policy.π(t.s)
             Zygote.@ignore @debug "policy training" a_probs
             a_sample_index = Zygote.@ignore sample(1:4, Weights(a_probs))
@@ -127,16 +144,25 @@ function train!(policy, replay_buffer)
             Q_term - entropy_term
         end
     end
+    @debug "policy loss" loss
     Flux.Optimise.update!(π_opt, π_params, grads)
+    polyak_average!(policy.q1_targ, policy.q1)
+    polyak_average!(policy.q2_targ, policy.q2)
 end
 
 struct Policy{P, Q} <: AbstractPolicy
-    π :: P
-    q1 :: Q
-    q2 :: Q
+    π       :: P
+    q1      :: Q
+    q2      :: Q
+    q1_targ :: Q
+    q2_targ :: Q
 end
 
-Policy() = Policy(make_π_network(), make_q_network(), make_q_network())
+function Policy()
+    q1 = make_q_network()
+    q2 = make_q_network()
+    Policy(make_π_network(), q1, q2, deepcopy(q1), deepcopy(q2))
+end
 
 function Reinforce.action(policy::AbstractPolicy, r, s, A)
     network_out::Vector{Float32} = policy.π(s::Union{Vector{Float32}, PyArray{Float32, 1}})
@@ -144,14 +170,19 @@ function Reinforce.action(policy::AbstractPolicy, r, s, A)
 end
 
 function main()
-    replay_buffer = CircularBuffer{SARSD}(10_000)
+    replay_buffer = CircularBuffer{SARSD}(100_000)
+    recent_episode_rewards = CircularBuffer{Float32}(100)
     policy = Policy()
-    for episode in 1:20
-        run_episode(env, policy) do (s, a, r, s′)
+    for episode in 1:200
+        episode_reward = run_episode(env, policy) do (s, a, r, s′)
             push!(replay_buffer, SARSD(s, a, Float32(r), s′, finished(env)))
             render(env)
         end
-        train!(policy, replay_buffer)
+        push!(recent_episode_rewards, episode_reward)
+        @info "" mean(recent_episode_rewards) length(replay_buffer)
+        for _ in 1:10
+            train!(policy, replay_buffer)
+        end
     end
 end
 
